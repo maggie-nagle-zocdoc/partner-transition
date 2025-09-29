abb80cfadf8a1e05b12a673871140ee5515374e5
maggienagle <maggie.nagle@zocdoc.com>
2025-09-29 15:44:19 -0400

    update copy and redirect link

--- script-auto-healthgrades.js
+++ script-auto-healthgrades.js
@@ -19,7 +19,7 @@ class AutoInterstitialPage {
     // Get redirect URL - specific Zocdoc booking URL
     getRedirectUrl() {
         // Base booking URL
-        const baseUrl = 'https://www.zocdoc.com/booking/patientinfo/03cfa413-5593-4b45-8142-bf70a808e18f';
+        const baseUrl = 'https://www.zocdoc.com/booking/patientinfo/e7876751-b906-4a9b-ae0c-65e638e9aa6b';
         
         // Add UTM parameters for tracking
         const utmParams = new URLSearchParams({
